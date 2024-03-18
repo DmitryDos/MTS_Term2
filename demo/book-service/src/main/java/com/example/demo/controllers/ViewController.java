53ded1d16e6a0bf635867a11dd8df68e10fc28ff
DmitryDos <dostovalov.ds@phystech.edu>
2024-03-18 21:17:28 +0300

    View controller fix

--- demo/book-service/src/main/java/com/example/demo/controllers/ViewController.java
+++ demo/book-service/src/main/java/com/example/demo/controllers/ViewController.java
@@ -7,13 +7,15 @@ import com.example.demo.service.AuthorService;
 import com.example.demo.service.BookService;
 import com.example.demo.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-
+@Controller
+@RequestMapping("/api")
 public class ViewController {
   private final BookService bookService;
   private final AuthorService authorService;
